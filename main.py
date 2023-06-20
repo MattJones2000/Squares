03da27b13e74a4ec5d6d30623698f63721e32277
Matbart1 <mathieubarrette9@gmail.com>
2023-06-20 17:51:16 -0600

    added really important info

--- main.py
+++ main.py
@@ -5,8 +5,8 @@
 
 def main():
 
-    print("hello world")
-    print('test')
+    print("hello world, it seems to stink of shit in here")
+    print('test..? test what, you just did hello world. fucking idiot.')
 
 if __name__ == "__main__":
     main()
